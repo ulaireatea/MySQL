974c08ba3b98484756d65db625284d6dafc94b08
Marcelo de Moura Cabral <63030264+ulaireatea@users.noreply.github.com>
2021-01-07 23:38:48 -0300

    more and more updates

---
+++ localhost.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Tempo de geração: 07/01/2021 às 22:55
+-- Versão do servidor: 10.4.17-MariaDB
+-- Versão do PHP: 8.0.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `exemplo`
+--
+DROP DATABASE IF EXISTS `exemplo`;
+CREATE DATABASE IF NOT EXISTS `exemplo` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `exemplo`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `amigos`
+--
+
+CREATE TABLE `amigos` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(30) NOT NULL,
+  `sexo` enum('M','F') NOT NULL,
+  `telefone` varchar(15) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Despejando dados para a tabela `amigos`
+--
+
+INSERT INTO `amigos` (`id`, `nome`, `sexo`, `telefone`) VALUES
+(1, 'Maria', 'F', '2222-3333'),
+(2, 'Joao', 'M', '1111-2222'),
+(3, 'Jose', 'M', '3333-4444');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `amigos`
+--
+ALTER TABLE `amigos`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nome` (`nome`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `amigos`
+--
+ALTER TABLE `amigos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
